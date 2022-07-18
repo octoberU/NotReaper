f5fe327329a20acf02cce754018b30cc7e573001
Wolf <wolferacing@gmail.com>
2022-07-17 21:10:24 -0400

    Logo swap for Discord and program icon
    Changed old logo to new logo

--- Assets/Scripts/NRDiscordPresence.cs
+++ Assets/Scripts/NRDiscordPresence.cs
@@ -19,6 +19,8 @@ namespace NotReaper {
 
         public DiscordPresence presence;
 
+        //original app id # 622577898775117825
+
         private void Start()
             => NRSettings.onSettingsSaved += (config) => gameObject.SetActive(config.useDiscordRichPresence);
         
